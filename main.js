441043f0e3ff1c3ec1b9cdcf0765deb8be4badfb
Adrian <adrianic92@yahoo.com>
2022-08-06 12:34:39 -0700

    commit tree 8f68ed0dd52b6787eb4237712324305e698b1b69

--- main.js
+++ main.js
@@ -4,8 +4,32 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const modal = document.getElementById("modal")
+modal.className = "hidden"
 
+const toggleHearts = document.getElementsByClassName("like-glyph");
 
+function heartCallback(element) {
+  const heart = element.target;
+  mimicServerCall("http://mimicServer.example.com")
+  .then(function() {
+    if (heart.innerText === EMPTY_HEART) {
+      heart.innerText = FULL_HEART;
+    }
+    else {
+      heart.innerText = EMPTY_HEART;
+    }
+  })
+  .catch(function(error) {
+    modal.className = "";
+    modal.innerText = error;
+    setTimeout(() => modal.className = "hidden", 3000);
+  })
+}
+
+for (const corazon of toggleHearts) {
+  corazon.addEventListener("click", heartCallback);
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
